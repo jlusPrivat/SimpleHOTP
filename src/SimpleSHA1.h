0a9ac40bd8346074880a564db9de35d1639a6571
Jonathan Lusky <jonathan@jlus.de>
2019-06-18 14:38:14 +0200

    SHA1 namespaced

--- src/SimpleSHA1.h
+++ src/SimpleSHA1.h
@@ -20,29 +20,25 @@ Simple SHA class generates a 160-bit SHA-1 Hash from an Array of 8-Bit Data
 
 #include "Arduino.h"
 
-class SimpleSHA1 {
-	private:
-		
-		/* Rotates a 32-bit integer left
-		 *
-		 * Param input: the integer to rotate
-		 * Param n: the number of places to rotate; max 32
-		 * Returns: the rotated integer
-		 */
-		static uint32_t rotL (uint32_t input, uint8_t n);
+namespace SimpleSHA1 {
+	/* Rotates a 32-bit integer left
+	 *
+	 * Param input: the integer to rotate
+	 * Param n: the number of places to rotate; max 32
+	 * Returns: the rotated integer
+	 */
+	uint32_t rotL (uint32_t input, uint8_t n);
 	
 	
-	public:
-		
-		/* Generates the Sha-1 Hash for a given message
-		 * 
-		 * Param message: An Array of Bytes containing the message
-		 * Param ml: the length of the message (in bits)
-		 * Param returner: an empty 5 element 32-bit array
-		 */
-		static void generateSha (uint8_t *message, uint64_t ml, uint32_t *returner);
-		
-};
+	/* Generates the Sha-1 Hash for a given message
+	 * 
+	 * Param message: An Array of Bytes containing the message
+	 * Param ml: the length of the message (in bits)
+	 * Param returner: an empty 5 element 32-bit array
+	 */
+	void generateSha (uint8_t *message, uint64_t ml, uint32_t *returner);
+	
+}
 
 
 #endif
